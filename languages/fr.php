a67828fd37da8a980a26cc4d7db5f81d51a60fcf
bm jade <benoit.mottin@jade.fr>
2017-10-02 17:22:28 +0200

    I18N

--- languages/fr.php
+++ languages/fr.php
@@ -1,29 +1,34 @@
 <?php
+/**
+ * This file was created by Translation Editor v5.2
+ * On 2017-10-02 17:21
+ */
 
-return array(
-	'etherpad' => "Co-création",
-	'etherpad:group' => "Co-création",
-	'etherpad:example' => "Exemple: https://beta.etherpad.org",
-	'group:pads' => "Co-créer un document de groupe",
-	'etherpad:friends' => "Contacts co-création",
-	'etherpad:url' => "URL du document",
-	'etherpad:add' => "Co-créer un document",
-	'etherpad:edit' => "Editer document",
-	'etherpad:objective' => "But",
-	'etherpad:none' => 'Pas de document disponible',
-	'etherpad:owner' => "Co-création",
-	'etherpad:everyone' => "Co-création",
-	'item:object:edocs' => "Co-création",
-	'river:create:object:etherpad' => "%s a créé(e) un document partagé %s",
-	'etherpad:river' => "Co-création",
-	'etherpad:unknown_pad' => "Co-création inconnue",
-	'etherpad:save:success' => "Co-création enregistrée",
-	'etherpad:save:failed' => "Erreur enregistrement Co-création",
-	'etherpad:register:no:saved' => "Co-création non enregistrée",
-	'etherpad:delete:success' => "Co-création supprimée",
-	'etherpad:delete:fail' => "Erreur suppression Co-création",
-	
-	// settings
-	'etherpad:base_url' => ' URL de base Etherpad',
-	'etherpad:example' => 'Exemple: https://beta.etherpad.org',		
+return array (
+  'etherpad' => 'Co-créer des documents',
+  'etherpad:group' => 'Co-créer des documents',
+  'etherpad:edit:with:etherpad' => 'Modifier le document',
+  'etherpad:edit:update:wiki' => 'Mettre à jours la page wiki',
+  'etherpad:base_port' => 'Port',
+  'etherpad:example:url' => 'https://etherpad.net',
+  'etherpad:example:port' => '9001',
+  'group:pads' => 'Co-créer un document de groupe',
+  'etherpad:friends' => 'Contacts co-création',
+  'etherpad:url' => 'URL du document',
+  'etherpad:add' => 'Co-créer un document',
+  'etherpad:edit' => 'Editer document',
+  'etherpad:objective' => 'But',
+  'etherpad:none' => 'Pas de document disponible',
+  'etherpad:owner' => 'Co-création',
+  'etherpad:everyone' => 'Co-création',
+  'item:object:edocs' => 'Co-création',
+  'river:create:object:etherpad' => '%s a créé(e) un document partagé %s',
+  'etherpad:river' => 'Co-création',
+  'etherpad:unknown_pad' => 'Co-création inconnue',
+  'etherpad:save:success' => 'Co-création enregistrée',
+  'etherpad:save:failed' => 'Erreur enregistrement Co-création',
+  'etherpad:register:no:saved' => 'Co-création non enregistrée',
+  'etherpad:delete:success' => 'Co-création supprimée',
+  'etherpad:delete:fail' => 'Erreur suppression Co-création',
+  'etherpad:base_url' => ' URL de base Etherpad',
 );
